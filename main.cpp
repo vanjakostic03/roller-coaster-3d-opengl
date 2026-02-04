1f0e91b6b1b3f63c4a7dfb4a07f6eeadec466e64
Vanja Kostic <vanjakostic03@gmail.com>
2026-02-04 21:41:39 +0100

    [ADD] - add blender models

--- main.cpp
+++ main.cpp
@@ -54,6 +54,9 @@ int main()
 
     Model lija("res/low-poly-fox.obj");
     Model tracks("res/tracks.obj");
+    Model car("res/car.obj");
+    Model seats("res/seats.obj");
+    Model human("res/human.obj");
     //Tjemena i baferi su definisani u model klasi i naprave se pri stvaranju objekata
 
     Shader unifiedShader("basic.vert", "basic.frag");
@@ -63,23 +66,24 @@ int main()
     unifiedShader.use();
 
     unifiedShader.setVec3("uLightPos1", 0, 100, 75);
-    unifiedShader.setVec3("uLightColor1", 5, 5,5);
+    unifiedShader.setVec3("uLightColor1", 2, 2,2);
 
     unifiedShader.setVec3("uLightPos2", 0, 100, 20);
-    unifiedShader.setVec3("uLightColor2", 5, 5, 5);
+    unifiedShader.setVec3("uLightColor2", 2, 2, 2);
 
     // Donji uglovi
     unifiedShader.setVec3("uLightPos3", 100, -20, -30);
-    unifiedShader.setVec3("uLightColor3", 5, 5, 5);
+    unifiedShader.setVec3("uLightColor3", 2, 2, 2);
 
     unifiedShader.setVec3("uLightPos4", 0, 0, 50);
-    unifiedShader.setVec3("uLightColor4", 5, 5, 5);
+    unifiedShader.setVec3("uLightColor4", 2, 2, 2);
 
     unifiedShader.setVec3("uViewPos", 0, 0, 5);
 
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
     unifiedShader.setMat4("uP", projection);
-    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 50.0f, -75.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 10.0f, -10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    //glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 20.0f, -60.0f), glm::vec3(0.0f, 10.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     unifiedShader.setMat4("uV", view);
     glm::mat4 model = glm::mat4(1.0f);
 
@@ -95,6 +99,9 @@ int main()
         unifiedShader.setMat4("uM", model);
         //lija.Draw(unifiedShader);
         tracks.Draw(unifiedShader);
+        car.Draw(unifiedShader);
+        seats.Draw(unifiedShader);
+        /*human.Draw(unifiedShader);*/
 
         glfwSwapBuffers(window);
         glfwPollEvents();
