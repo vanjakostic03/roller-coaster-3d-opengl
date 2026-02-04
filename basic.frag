d642c1319cbde470f6b6205ae51b2cd8d131ea98
Vanja Kostic <vanjakostic03@gmail.com>
2026-02-04 20:20:24 +0100

    [ADD] - new project

---
+++ basic.frag
@@ -0,0 +1,64 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 chNormal;
+in vec3 chFragPos;
+in vec2 chUV;
+
+uniform vec3 uLightPos1;
+uniform vec3 uLightColor1;
+
+uniform vec3 uLightPos2;
+uniform vec3 uLightColor2;
+
+uniform vec3 uLightPos3;
+uniform vec3 uLightColor3;
+
+uniform vec3 uLightPos4;
+uniform vec3 uLightColor4;
+
+uniform vec3 uLightPos5;
+uniform vec3 uLightColor5;
+
+uniform vec3 uLightPos6;
+uniform vec3 uLightColor6;
+
+uniform vec3 uViewPos;
+uniform sampler2D uDiffMap1;
+
+vec3 calcLight(vec3 lightPos, vec3 lightColor, vec3 normal)
+{
+    // ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // diffuse
+    vec3 lightDir = normalize(lightPos - chFragPos);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    // specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(uViewPos - chFragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    return ambient + diffuse + specular;
+}
+
+void main()
+{
+    vec3 norm = normalize(chNormal);
+
+    vec3 light1 = calcLight(uLightPos1, uLightColor1, norm);
+    vec3 light2 = calcLight(uLightPos2, uLightColor2, norm);
+    vec3 light3 = calcLight(uLightPos2, uLightColor2, norm);
+    vec3 light4 = calcLight(uLightPos2, uLightColor2, norm);
+    vec3 light5 = calcLight(uLightPos2, uLightColor2, norm);
+    vec3 light6 = calcLight(uLightPos2, uLightColor2, norm);
+
+    vec3 result = light1 + light2 + light3 + light4;
+
+    FragColor = texture(uDiffMap1, chUV) * vec4(result, 1.0);
+}
---
+++ main.cpp
@@ -0,0 +1,107 @@
+//Vanja Kostic SV/2022
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader.hpp"
+#include "model.hpp"
+
+
+
+int main()
+{
+    if(!glfwInit())
+    {
+        std::cout << "GLFW fail!\n" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+    int screenWidth = mode->width;
+    int screenHeight = mode->height;
+
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "OpenGL Window", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Window fail!\n" << std::endl;
+        glfwTerminate();
+        return -2;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() !=GLEW_OK)
+    {
+        std::cout << "GLEW fail! :(\n" << std::endl;
+        return -3;
+    }
+
+
+    Model lija("res/low-poly-fox.obj");
+    Model tracks("res/tracks.obj");
+    //Tjemena i baferi su definisani u model klasi i naprave se pri stvaranju objekata
+
+    Shader unifiedShader("basic.vert", "basic.frag");
+
+    //Render petlja
+    unifiedShader.use();
+    unifiedShader.use();
+
+    unifiedShader.setVec3("uLightPos1", 0, 100, 75);
+    unifiedShader.setVec3("uLightColor1", 5, 5,5);
+
+    unifiedShader.setVec3("uLightPos2", 0, 100, 20);
+    unifiedShader.setVec3("uLightColor2", 5, 5, 5);
+
+    // Donji uglovi
+    unifiedShader.setVec3("uLightPos3", 100, -20, -30);
+    unifiedShader.setVec3("uLightColor3", 5, 5, 5);
+
+    unifiedShader.setVec3("uLightPos4", 0, 0, 50);
+    unifiedShader.setVec3("uLightColor4", 5, 5, 5);
+
+    unifiedShader.setVec3("uViewPos", 0, 0, 5);
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)screenWidth / (float)screenHeight, 0.1f, 100.0f);
+    unifiedShader.setMat4("uP", projection);
+    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 50.0f, -75.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    unifiedShader.setMat4("uV", view);
+    glm::mat4 model = glm::mat4(1.0f);
+
+    glEnable(GL_DEPTH_TEST);
+    while (!glfwWindowShouldClose(window))
+    {
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        //model = glm::rotate(model, glm::radians(0.1f), glm::vec3(0.0f, 1.0f, 0.0f));
+        unifiedShader.setMat4("uM", model);
+        //lija.Draw(unifiedShader);
+        tracks.Draw(unifiedShader);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+
